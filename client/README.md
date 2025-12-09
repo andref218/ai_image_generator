82b45bc57bba0a4769ff6facf7b452bc8aeebeb5
Andre Fonseca <andrefonseca.aff@gmail.com>
2025-12-09 17:27:02 +0000

    Initial commit

---
+++ client/README.md
@@ -0,0 +1,3 @@
+# AI Image Generator
+
+An application to generate stunning images from text prompts using artificial intelligence.
---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      AI Image Generator App
+    </div>
+  );
+};
+
+export default App;
